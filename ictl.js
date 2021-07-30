23c270421518ac54bb7b40c5ceadaed99cfbf999
nodai2hITC <nodai2h.itc@gmail.com>
2021-07-30 22:42:36 +0900

    add formatter

--- ictl.js
+++ ictl.js
@@ -4,10 +4,10 @@ var ICTL = ICTL || {};
 
 ICTL.default_array_size = 26;
 
-(function(){
-    ICTL.compile = function(program){
+(function() {
+    ICTL.compile = function(program) {
         // 行番号を除去
-        program = program.replace(/^\(\d+\)\s*/mg, "");
+        program = program.replace(/^\(\d+\) */mg, "");
         // ｜を除去
         while (program.match(/^(\s*)｜/m)) {
             program = program.replace(/^(\s*)｜/mg, "$1  ");
@@ -19,11 +19,11 @@ ICTL.default_array_size = 26;
         }
 
         // 「もし〇ならば:」を「if (〇) {」に変換
-        program = program.replace(/もし(.+?)ならば[:：]$/mg, function(m, p1){
+        program = program.replace(/もし(.+?)ならば[:：]$/mg, function(m, p1) {
             return "if (" + replace_and_or(p1) + ") {";
         });
         // 「あるいは〇ならば:」を「} else if (〇) {」に変換
-        program = program.replace(/あるいは(.+?)ならば[:：]$/mg, function(m, p1){
+        program = program.replace(/あるいは(.+?)ならば[:：]$/mg, function(m, p1) {
             return "} else if (" + replace_and_or(p1) +") {";
         });
         // 「そうでなければ:」を「} else {」に変換
@@ -33,14 +33,14 @@ ICTL.default_array_size = 26;
         program = program.replace(/(\w+)\s*を\s*(.+?)\s*から\s*(.+?)\s*まで\s*(.+?)\s*ずつ増やしながら(?:繰り返す)?[:：]$/mg,
                                 "for ($1 = $2; $1 <= $3; $1 += $4) {");
         // 「〇の間繰り返す:」を「while (〇) {」に変換
-        program = program.replace(/(.+?)の間繰り返す[:：]$/mg, function(m, p1){
+        program = program.replace(/(.+?)の間繰り返す[:：]$/mg, function(m, p1) {
             return "while (" + replace_and_or(p1) + ") {";
         });
 
         // 「配列変数 〇 を初期化する」を「〇 = [];」に変換
         program = program.replace(/配列変数\s*(\w+)\s*を初期化する$/mg, "$1 = [];");
         // 「要素数□の配列 〇 のすべての要素に △ を代入する」を「〇 = []; for(let i = 0; i < □; i++) { 〇[i] = △; }」に変換
-        program = program.replace(/(?:要素数(.+)の)?配列\s*(\w+)\s*のすべての要素に\s*(.+?)\s*を代入する$/mg, function(m, p1, p2, p3){
+        program = program.replace(/(?:要素数(.+)の)?配列\s*(\w+)\s*のすべての要素に\s*(.+?)\s*を代入する$/mg, function(m, p1, p2, p3) {
             return p2 + " = []; " + 
                 "for(let i=0; i < " + (p1 ? p1 : "ICTL.default_array_size") + "; i++) " +
                 "{ " + p2 + "[i] = " + p3 + "; }";
@@ -48,9 +48,9 @@ ICTL.default_array_size = 26;
 
         // 配列の初期化処理の追加
         let arrays = program.match(/(\w+)\[/g);
-        if (arrays){
+        if (arrays) {
             let keys = {};
-            for(let i = 0; i < arrays.length; i++){
+            for (let i = 0; i < arrays.length; i++) {
                 if (keys[arrays[i]]) continue;
                 program = arrays[i].replace("[", "=[];") + program;
                 keys[arrays[i]] = true;
@@ -59,8 +59,33 @@ ICTL.default_array_size = 26;
         return program;
     };
 
+    ICTL.format = function(program) {
+        program = program.replace(/^(?:\(\d+\))? */mg, "");
+        while (program.match(/^｜ */m)) {
+            program = program.replace(/^｜ */mg, "");
+        }
+        program = program.replace(/[\r\n]+$/, "");
+        let lines = program.split(/\r?\n/);
+        let indent = 0;
+        let new_program = "";
+        let digits = 2;
+        if (lines.length >= 100) digits = lines.length.toString().length;
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i];
+            indent += 1 - line.split("⎿").length;
+            if (line.match(/あるいは(.+?)ならば[:：]$/m) ||
+                line.match(/そうでなければ[:：]$/m)) indent--;
+            if (indent < 0) indent = 0;
+            new_program += "(" + ("0000000000" + (i + 1).toString()).slice(-digits) + ")";
+            for (let j = 0; j < indent; j++) new_program += "｜ ";
+            new_program += line + "\n";
+            if (line.match(/[:：]$/)) indent++;
+        }
+        return new_program;
+    };
+
     // and, or を &&, || に変換
-    function replace_and_or(condition){
+    function replace_and_or(condition) {
         return condition.replace(/ and /g, " && ").replace(/ or /g, " || ");
     };
 })();
@@ -84,7 +109,7 @@ function 切り捨て(f) {
 
 function 表示する() {
     let str = "";
-    for(let i=0; i<arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++){
         str += arguments[i].toString();
     }
     document.getElementById("output").value += str + "\n";
